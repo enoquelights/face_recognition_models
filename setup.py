e5de28df0a7278f35faec191faf85037f9964101
enoquelights <84310653+enoquelights@users.noreply.github.com>
2025-06-21 10:39:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.13',
     ],
     test_suite='tests',
     tests_require=test_requirements
